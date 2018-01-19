ff3a7165cfd4e78f0d8055512f6e7eb8d5b53c2d
Craig Porteous <Craig.Porteous@gmail.com>
2018-01-19 14:45:06 +0000

    Fixed connection error and use of missing variable

--- scripts/Restore-RSDatabase.ps1
+++ scripts/Restore-RSDatabase.ps1
@@ -26,7 +26,7 @@ Write-Information "Migrate the DB using dbaTools"
 try {
     Write-Verbose "Restoring Database from $($backupLocation)"
     Restore-DbaDatabase -SqlServer $targetInstance -Path $backupLocation -WithReplace
-    Write-Verbose "Database restored to $($targetDatabase) on $($targetServer)"
+    Write-Verbose "Database restored to $($targetDatabase) on $($targetInstance)"
 
 }
 catch {
@@ -40,7 +40,10 @@ Write-Information "Set RS Database"
 try {
     Write-Verbose "Get SQL Version for $($targetInstance)"
     $SQLVersion = Get-DbaSqlInstanceProperty -SqlInstance $targetInstance | Where-Object{$_.Name -eq 'VersionMajor'}
-    Write-Verbose "Setting RS Database for $($targetInstance)"
+    Write-Verbose "Connecting to $($reportServer)'s default instance"
+    Connect-RsReportServer -ComputerName $ssrsServer -ReportServerInstance 'MSSQLSERVER' -ReportServerUri $reportServer
+    
+    Write-Verbose "Setting up RS Database $($targetDatabase) Version:$($SQLVersion.Value) on $($targetInstance)"
     Set-RsDatabase -DatabaseServerName $targetInstance -Name $targetDatabase -IsExistingDatabase -DatabaseCredentialType ServiceAccount -ReportServerVersion $SQLVersion.Value
 }
 catch {
@@ -52,8 +55,6 @@ Write-Information "Restore the Encryption Key"
 #-----------------------------------------------------------------
 
 try{
-    Write-Verbose "Connecting to $($reportServer)"
-    Connect-RsReportServer -ComputerName $ssrsServer -ReportServerInstance 'MSSQLSERVER' -ReportServerUri $reportServer
     Write-Verbose "Restoring Encryption Key from Source"
     $encKey = Read-Host "Please provide local encryption key for Database Source (E.g. C:\Encrypt_Key.snk)"
     $encKeyPass = Read-Host "Please provide the Password for the Encryption Key"
